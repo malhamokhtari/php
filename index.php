9ab228008a695b49773e21452054e75400ba2634
malhamokhtari <111632549+malhamokhtari@users.noreply.github.com>
2022-11-09 12:14:26 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$user = "caux";
+$password = "caux";
+$database = "caux";
+$table = "employee";
+
+try {
+
+        $db= new PDO("pgsql:host=192.168.40.132;dbname=$database", $user, $password);
+
+  foreach($db->query("SELECT * FROM $table") as $row) {
+
+echo "<table style='background-color:#D3D3D3' width=30% border='1'>
+
+<tr>
+<th <td>first_name</th></td>
+<th>last_name</th>
+<th >city_name</th>
+<th width>email</th>
+</tr>";
+
+echo "<tr>";
+echo "<td >" . $row['first_name'] . "</td>";
+echo "<td >" . $row['last_name'] . "</td>";
+echo "<td >" . $row['city_name'] . "</td>";
+echo "<td >" . $row['email'] . "</td>";
+echo "</tr>";
+echo "<table>";
+
+  }
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
